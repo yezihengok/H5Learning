1ad1c271b2c0680edc03e1430ac2955b134321e6
yzh <295658413@qq.com>
2021-05-26 18:16:46 +0800

    node.js 使用

--- 6-node.js/02/files/test.js
+++ 6-node.js/02/files/test.js
@@ -2,6 +2,10 @@
 console.log('加载了test这个用户自定义模块')
 
 
+// 什么是模块作用域
+// 和函数作用域类似， 在自定义模块中定义的变量、 方法等成员， 只能在当前模块内被访问， 这种模块级别的访问限制， 叫做模块
+// 作用域。
+
 const username = '张三'
 function sayHello() {
     console.log('大家好，我是' + username)
--- 6-node.js/02/files/test3.js
+++ 6-node.js/02/files/test3.js
@@ -12,3 +12,5 @@ exports.sayHello = function() {
 }
 
 // 向外共享的结果，最终都是 module.exports 所指向的对象
+
+// 注意： 为了防止混乱， 建议尽量不要在同一个模块中同时使用 exports 和 module.exports
